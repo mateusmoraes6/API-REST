ac914bbf8ee04d366111d16bfc243ff4ea00f704
Mateus Moraes <mateusjr_m.a@hotmail.com>
2024-03-22 16:29:34 -0300

    .env

--- src/app/database/conexão.js
+++ src/app/database/conexão.js
@@ -1,11 +1,14 @@
-import mysql from "mysql";
+import mysql from "mysql2";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const conexao = mysql.createConnection({
-  host: "localhost",
-  port: "3306",
-  user: "mateusjrma",
-  password: "adm767mat",
-  database: "bd_copa",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 conexao.connect();
